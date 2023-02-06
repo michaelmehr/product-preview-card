e5f5dca1e2993c8d9ae082b7f331f106d3a9c841
michaelmehr <michaelmehr0@gmail.com>
2023-02-06 15:07:40 -0600

    added basic unstyled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <div>
+        <img></img>
+        <div>
+          <p>Perfume</p>
+          <p>Gabrielle Essence Eau De Parfum</p>
+          <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <p>$149.99<span>$169.99</span></p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
     </div>
   )
 }
